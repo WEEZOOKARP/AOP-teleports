b39f57319317e0060a62c841bba4d8a0ed2cce22
WEEZOOKA <76672483+WEEZOOKARP@users.noreply.github.com>
2025-12-12 16:20:13 +1300

    Update Config layout in preparation for gotoaop cmd update

--- config.lua
+++ config.lua
@@ -1,17 +1,17 @@
 Config = {
-    {
+    ["sandy"] = {
         label = "Sandy Shores",
         from = vector3(-1037.65, -2737.34, 20.17),
         to = vector3(1841.32, 3668.57, 33.68),
         colour = {r = 255, g = 10, b = 10, a = 150}, -- red , green, blue, alpha 
     },
-    {
+    ["paleto"] = {
         label = "Paleto Bay",
         from = vector3(-1033.5, -2737.25, 20.17),
         to = vector3(176.63, 6636.14, 31.63),
         colour = {r = 255, g = 10, b = 200, a = 150}, -- red , green, blue, alpha 
     },
-    {
+    ["mirrorpark"] = {
         label = "Mirror Park",
         from = vector3(-1035.12, -2740.52, 20.17),
         to = vector3(1129.01, -646.23, 56.83),
